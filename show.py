eaa0bd78d5769c0f0978b7c2253d19c321bb2a5c
adusachev <usachev.alexe@gmail.com>
2024-02-05 12:00:40 +0300

    fixes

--- show.py
+++ show.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 def my_plot(x, y):
-    plt.plot(x, y, '*g')
+    plt.plot(x, y, marker="*", color="C1")
     plt.title("new plot title")
     plt.grid()
     plt.show()
